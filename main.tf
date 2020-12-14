c6c6a92d7175737a25dee9018c0c45bb2e59ff47
Chuck Cox <chuckc@us.ibm.com>
2020-12-14 12:22:27 -0600

    new: tekton vars

--- main.tf
+++ main.tf
@@ -3,15 +3,17 @@ resource "null_resource" "create_kubernetes_toolchain" {
     command = "${path.cwd}/.terraform/modules/ibm-kubernetes-toolchain/scripts/create-toolchain.sh"
 
     environment={
-      REGION     =  var.region
-      TOOLCHAIN_TEMPLATE_REPO  = var.toolchain_template_repo
-      APPLICATION_REPO = var.application_repo
-      RESOURCE_GROUP = var.resource_group
-      API_KEY = var.ibmcloud_api_key
-      CLUSTER_NAME = var.cluster_name
+      REGION            =  var.region
+      TOOLCHAIN_TEMPLATE_REPO = var.toolchain_template_repo
+      APPLICATION_REPO  = var.application_repo
+      RESOURCE_GROUP    = var.resource_group
+      API_KEY           = var.ibmcloud_api_key
+      CLUSTER_NAME      = var.cluster_name
       CLUSTER_NAMESPACE = var.cluster_namespace
       CONTAINER_REGISTRY_NAMESPACE = var.container_registry_namespace
-      TOOLCHAIN_NAME = var.toolchain_name
+      TOOLCHAIN_NAME    = var.toolchain_name
+      PIPELINE_TYPE     = var.pipeline_type
+      BRANCH            = var.branch
     }
   } 
 }
